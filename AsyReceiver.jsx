d920e5c8e234d056971f225e61f714972e9a97ea
icp049 <ianpepe2000@gmail.com>
2023-08-15 18:17:56 -0600

    edited

--- AsyReceiver.jsx
+++ AsyReceiver.jsx
@@ -11,13 +11,13 @@ MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMsYHhXEnfN2LKZz4dyGzpwnOu37mJW7
 N23E2Uny4RdCkCeN9qAt9dcmE5NYzSvR0SZ9ZmNJn9+9THmYdZUaG9kCAwEAAQ==
 -----END PUBLIC KEY-----`;
 
-const myText = 'secret message';
+const myText = 'secret message is inside';
 
-const encryptedData = crypto.publicEncrypt(receiverPublicKey, Buffer.from(myText, 'utf8'));
+const encryptedData = crypto.publicEncrypt(senderPublicKey, Buffer.from(myText, 'utf8'));
 console.log ('Encrypt:', encryptedData.toString('base64'));
 
 
 const sign = crypto.createSign('RSA-SHA256');
 sign.update(myText);
-const signature = sign.sign(senderKeyPair.privateKey, 'base64');
+const signature = sign.sign(receiverKeyPair.privateKey, 'base64');
 console.log('Signature: ', signature);
