56aa46d2b90b00bd7aa0cc30bbe85d9bffeafa18
icp049 <ianpepe2000@gmail.com>
2023-08-15 18:16:11 -0600

    edited

---
+++ AsyReceiver.jsx
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+const receiverKeyPair = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding : {type: 'spki', format: 'pem'},
+    privateKeyEncoding: {type: 'pkcs8', format: 'pem'}
+});
+
+const senderPublicKey  = `-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMsYHhXEnfN2LKZz4dyGzpwnOu37mJW7
+N23E2Uny4RdCkCeN9qAt9dcmE5NYzSvR0SZ9ZmNJn9+9THmYdZUaG9kCAwEAAQ==
+-----END PUBLIC KEY-----`;
+
+const myText = 'secret message';
+
+const encryptedData = crypto.publicEncrypt(receiverPublicKey, Buffer.from(myText, 'utf8'));
+console.log ('Encrypt:', encryptedData.toString('base64'));
+
+
+const sign = crypto.createSign('RSA-SHA256');
+sign.update(myText);
+const signature = sign.sign(senderKeyPair.privateKey, 'base64');
+console.log('Signature: ', signature);
