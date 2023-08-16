b54ae400a2fdf370a25d28e6a758978dd67caab8
icp049 <ianpepe2000@gmail.com>
2023-08-15 18:31:02 -0600

    edited

--- Keypairs.jsx
+++ Keypairs.jsx
@@ -3,7 +3,7 @@ const {generateKeyPairSync} = require('crypto');
 // this is a shared key
 //private to public
 
-const {privateKeym publicKey} = generateKeyPairSync('rsa', {
+const {privateKey, publicKey} = generateKeyPairSync('rsa', {
 modulusLength: 2048,
 publicKeyEncoding :{
     type: 'spki',
